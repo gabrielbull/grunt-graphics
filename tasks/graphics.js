d52faa14fa3c31444c218e73dc849baee3778075
Gabriel Bull <me@gabrielbull.com>
2015-05-04 18:37:12 -0400

    Added inital js structure

---
+++ tasks/graphics.js
@@ -0,0 +1,23 @@
+/* global module:false */
+/* global process:false */
+/** @param {Object} grunt Grunt. */
+module.exports = function(grunt) {
+    grunt.registerTask('graphics', function () {
+        var done = this.async();
+        var queue = 0;
+        var task = {
+            start: function () {
+                queue++;
+            },
+            end: function () {
+                queue--;
+                if (queue === 0) {
+                    done();
+                }
+            }
+        };
+
+        var GraphicsController = require('./../src/graphics-controller');
+        new GraphicsController(task, process, grunt).process(grunt.config.get('graphics')[this.args[0]].src);
+    });
+};
